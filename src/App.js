d14c9351064771135f3e1567072f42a188e9f25f
Chinwenma <okoriechinwe1327@gmail.com>
2023-04-02 23:08:33 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './index.css';
 import Header from './Components/Header';
 import Contact from './Components/Contact';
+
 export default function App() {
   return (
     <div className="App">
-
     <Header />
     <Contact />
     </div>
